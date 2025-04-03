435805f433cd88a2d421f7161e43d760dc8158f7
Theo <gg46ixav@studserv.uni-leipzig.de>
2025-04-03 13:45:35 +0200

    updated mappings

--- src/main/java/me/einundneunzig/songquizapi/RestController.java
+++ src/main/java/me/einundneunzig/songquizapi/RestController.java
@@ -8,12 +8,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
 import java.util.Set;
 
 @org.springframework.web.bind.annotation.RestController
 @CrossOrigin
+@RequestMapping("/songquizapi")
 public class RestController {
     private final SongRepository songRepository;
 
@@ -22,13 +24,13 @@ public class RestController {
         this.songRepository = songRepository;
     }
 
-    @GetMapping
+    @GetMapping("/getRandomSongByGenres")
     public ResponseEntity<Song> getRandomSongByGenres(@Param("genres") Set<String> genres) {
         Optional<Song> opt = songRepository.findRandomSongByGenres(genres);
         return opt.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @GetMapping ResponseEntity<Set<String>> getAllGenres() {
+    @GetMapping("getAllGenres") ResponseEntity<Set<String>> getAllGenres() {
         Set<String> set = songRepository.findAllGenres();
         return new ResponseEntity<>(set, HttpStatus.OK);
     }
