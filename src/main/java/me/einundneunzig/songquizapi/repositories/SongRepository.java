8862fe674349c7ad5a1e6d0dcbe85d6b324107e5
Theo <gg46ixav@studserv.uni-leipzig.de>
2025-04-14 13:22:10 +0200

    updated genres rest

--- src/main/java/me/einundneunzig/songquizapi/repositories/SongRepository.java
+++ src/main/java/me/einundneunzig/songquizapi/repositories/SongRepository.java
@@ -10,16 +10,16 @@ import java.util.Set;
 
 public interface SongRepository extends JpaRepository<Song, Long> {
 
-    @Query(value = "SELECT * FROM song WHERE language = :language AND genre_name @> :genres AND popularity >= :popularity ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM song, song_genres WHERE song.id = song_genres.song_id AND language = :language AND genres IN :genres AND popularity >= :popularity ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
     Optional<Song> findRandomSongByLanguageGenresAndPopularity(@Param("language") String language, @Param("genres") Set<String> genres, @Param("popularity") int popularity);
 
-    @Query(value = "SELECT * FROM song WHERE language = :language AND genre_name @> :genres ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM song, song_genres WHERE song.id = song_genres.song_id AND language = :language AND genres IN :genres ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
     Optional<Song> findRandomSongByLanguageAndGenres(@Param("language") String language, @Param("genres") Set<String> genres);
 
     @Query(value = "SELECT * FROM song WHERE language = :language ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
     Optional<Song> findRandomSongByLanguage(@Param("language") String language);
     
-    @Query(value = "SELECT * FROM song WHERE genre_name @> :genres ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM song, song_genres WHERE song.id = song_genres.song_id AND genres IN :genres ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
     Optional<Song> findRandomSongByGenres(@Param("genres") Set<String> genres);
     
     @Query(value = "SELECT * FROM song ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
