5634cc75affa9889cadb1fe56afd7bf6fb1cd483
einundneunzig <102417643+einundneunzig@users.noreply.github.com>
2025-05-15 19:54:31 +0000

    Update RestController.java

--- src/main/java/me/einundneunzig/songquizapi/RestController.java
+++ src/main/java/me/einundneunzig/songquizapi/RestController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
 import java.util.Set;
+import java.util.List;
 
 @org.springframework.web.bind.annotation.RestController
 @CrossOrigin
